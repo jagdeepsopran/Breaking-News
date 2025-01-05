abf42dea86699e6518504e2c2782e71d282cb1d5
Jagdeep <jagdeepsopran@gmail.com>
2025-01-05 13:53:05 +0530

    import css

--- src/component/individual/News.jsx
+++ src/component/individual/News.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import './news.css'
+import './component/individual/news.css'
 const News = ({ data }) => {
     let { index } = useParams();
     return (

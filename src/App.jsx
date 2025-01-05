004c7da80cd05ebfc764d562644285c280516363
Jagdeep <jagdeepsopran@gmail.com>
2025-01-05 13:36:16 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import Home from './component/Home'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import News from './component/individual/News'
+import React, { useEffect, useState } from "react";
+import Home from "./component/Home";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import News from "./component/individual/News";
 
 const App = () => {
   let [data, setData] = useState();
   useEffect(() => {
     async function news() {
-      const res = await fetch('https://newsapi.org/v2/everything?q=apple&from=2024-12-09&to=2024-12-09&sortBy=popularity&apiKey=2a69ad5a546e4f8dbbd36c5a3d2571c9');
+      const res = await fetch(
+        "https://newsapi.org/v2/everything?q=apple&from=2024-12-09&to=2024-12-09&sortBy=popularity&apiKey=2a69ad5a546e4f8dbbd36c5a3d2571c9"
+      );
       const response = await res.json();
       console.log(response);
       setData(response);
@@ -23,7 +25,7 @@ const App = () => {
         {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
